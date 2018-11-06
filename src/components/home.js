c352c66c78369eceaa86000f23c5437135b56080
Ibrahim El Gohary <goharyibrahim@gmail.com>
2018-11-06 18:26:18 +0200

    Complete

--- src/components/home.js
+++ src/components/home.js
@@ -3,110 +3,123 @@ import React, { Component } from 'react';
 import "./home.css";
 
 class Home extends React.Component {
-   constructor(props){
-       super(props);
-       this.state={
-           newItem:"",
-         allItems:["asss","ddddd","sxcddd"],
-           searchResults:[],
-           notsearching: true,
-           editClicked: false
-       };
+    constructor(props){
+        super(props);
+        this.state={
+            newItem:"",
+            allItems:["asss","ddddd","sxcddd"],
+            searchResults:[],
+            notsearching: true,
+            editClicked: false
+        };
 
-       this.addItem = this.addItem.bind(this);
-       this.search = this.search.bind(this);
-       this.handleChange=this.handleChange.bind(this);
-       this.remove = this.remove.bind(this);
-       this.edit = this.edit.bind(this);
-       this.editToggle = this.editToggle.bind(this);
-   }
+        this.addItem = this.addItem.bind(this);
+        this.search = this.search.bind(this);
+        this.handleChange=this.handleChange.bind(this);
+        this.remove = this.remove.bind(this);
+        this.edit = this.edit.bind(this);
+        this.editToggle = this.editToggle.bind(this);
+        this.editFromSearch = this.editFromSearch.bind(this);
+    }
 
-   addItem(){
+    addItem(){
         this.setState({notsearching: true});
-       let allItemsArray = [...this.state.allItems];
-       allItemsArray.push(this.state.newItem);
-       this.setState({allItems: allItemsArray});
-   }
+        let allItemsArray = [...this.state.allItems];
+        allItemsArray.push(this.state.newItem);
+        this.setState({allItems: allItemsArray});
+    }
 
-   search(event){
-       this.setState({notsearching: false});
-       const searchInput = event.target.value.toUpperCase();
+    search(event){
+        this.setState({notsearching: false});
+        const searchInput = event.target.value.toUpperCase();
 
-       const searchResults = [];
+        const searchResults = [];
 
-       this.state.allItems.map((item)=>{
-           if(item.toUpperCase().indexOf(searchInput)> -1) {
-               searchResults.push(item);
-               this.setState({searchResults: searchResults});
-           }});
+        this.state.allItems.map((item)=>{
+            if(item.toUpperCase().indexOf(searchInput)> -1) {
+                searchResults.push(item);
+                this.setState({searchResults: searchResults});
+            }});
 
 
-           }
+    }
 
-   handleChange(event){
-       this.setState({newItem: event.target.value});
-   }
+    handleChange(event){
+        this.setState({newItem: event.target.value});
+    }
 
-   remove(i){
-       let arrayModifier = [...this.state.allItems];
-       arrayModifier.splice(i, 1);
-       this.setState({allItems: arrayModifier});
-   }
+    remove(i){
+        let arrayModifier = [...this.state.allItems];
+        arrayModifier.splice(i, 1);
+        this.setState({allItems: arrayModifier});
+    }
 
-   edit(e, i){
-       let itemToEdit = e.target.value;
-       let arrayAfterEdit = [...this.state.allItems];
-       arrayAfterEdit[i] = itemToEdit;
-       this.setState({allItems: arrayAfterEdit});
-       
-   }
+    edit(e, i){
+        let itemToEdit = e.target.value;
+        let arrayAfterEdit = [...this.state.allItems];
+        arrayAfterEdit[i] = itemToEdit;
+        this.setState({allItems: arrayAfterEdit});
 
-   editToggle(){
-       this.setState({editClicked: !this.state.editClicked});
-   }
+    }
+
+    editFromSearch(e,i){
+        let itemToEdit = e.target.value;
+        let arrayAfterEdit = [...this.state.searchResults];
+        arrayAfterEdit[i] = itemToEdit;
+        this.setState({searchResults: arrayAfterEdit});
+    }
+
+    editToggle(){
+        this.setState({editClicked: !this.state.editClicked});
+    }
 
     render() {
-      let  notsearching = this.state.notsearching;
-      let editClicked = this.state.editClicked;
+        let  notsearching = this.state.notsearching;
+        let editClicked = this.state.editClicked;
         return (
             <div>
-            <header>
-                <h1>React To-Do App</h1>
-                <h3>Enhance your productivity</h3>
-            </header>
+                <header>
+                    <h1>React To-Do App</h1>
+                    <h3>Enhance your productivity</h3>
+                </header>
                 <div className="home-input">
-                <input type="text" placeholder="create new work item" value={this.state.newItem}
-                       onChange={this.handleChange}/>
+                    <input type="text" placeholder="create new work item" value={this.state.newItem}
+                           onChange={this.handleChange}/>
                     <button className="button" onClick={this.addItem}>Enter New Item</button>
-                <input type="text"  placeholder="filter list" onChange={this.search} />
+                    <input type="text"  placeholder="filter list" onChange={this.search} />
                 </div>
                 {notsearching ? (this.state.allItems.map((item,index)=>
-                { return(
+                    { return(
 
-                    <div>
-                        {editClicked ? null : (<span>Task {index + ":"}{item}</span>)}
+                        <div>
+                            {editClicked ? null : (<span>Task {index + ":"}{item}</span>)}
 
-                    <button onClick={()=> {this.remove(index)}}>Delete</button>
-                        <button onClick={this.editToggle}>Edit</button>
+                            <button onClick={()=> {this.remove(index)}}>Delete</button>
+                            <button onClick={this.editToggle}>Edit</button>
 
-                        {editClicked ? (<input  value={item} onChange={(e) =>
-                        this.edit(e,index)}/>) : null}
+                            {editClicked ? (<input  value={item} onChange={(e) =>
+                                this.edit(e,index)}/>) : null}
 
-                    </div>
-                );
-                })):
+                        </div>
+                    );
+                    })):
                     (this.state.searchResults.map((item,index)=>{
-                        return(
-                            <div>
-                                Task {index + ":"}{item}
-                                <button onClick={()=> {this.remove(index)}}>Delete</button>
-                                <button onClick={this.editToggle}>Edit</button>
-                            </div>
+                            return(
+                                <div>
+                                    {editClicked ? null : (<span>Task {index + ":"}{item}</span>)}
+
+                                    <button onClick={()=> {this.remove(index)}}>Delete</button>
+                                    <button onClick={this.editToggle}>Edit</button>
+
+                                    {editClicked ? (<input  value={item} onChange={(e) =>
+                                        this.editFromSearch(e,index)}/>) : null}
+
+                                </div>
 
 
-                        );
-                    })
-                ) }
+                            );
+                        })
+                    ) }
 
             </div>
 
@@ -118,24 +131,3 @@ export default Home;
 
 
 
-/*
-header react to do app
-enhance....
-
-input create
-
-input enter new item
-
-input filter list
-
-list
-list item: has a checkbox
-has 3 columns
-task + number
-the task itself
-checkbox
-
-2 buttons:
-clear the list
-reset the list
- */
